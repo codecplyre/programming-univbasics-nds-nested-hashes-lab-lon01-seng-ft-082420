aeb35e4fe41fd93a7d942573e5c7a454ad00883a
Ricky Adriell Cultura <pansoy.rickyadriell26457@gmail.com>
2020-03-13 15:43:38 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -21,7 +21,7 @@ end
 def literal_aoh
   # Using Array literal syntax only, build a nested array that uses the data in
   # held in the constants
-  A =  [
+  A = [
     DON_G,
     JOELLE_VD,
     PAT_M,
